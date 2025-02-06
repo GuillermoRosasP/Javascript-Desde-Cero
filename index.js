db733e576a7a56dd3909e5255560b0cfe0a21117
Guillermo Rosas <memorosaspalma@gmail.com>
2025-02-05 22:08:16 -0600

    javascript desde cero 2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 console.log("Hola desde JS");
 alert("Tu Pasword es Incorrecta")
 
@@ -7,7 +8,7 @@ var gusto1= "Jugar"
 var gusto2="hacer ejercicio"
 console.log(typeof nombre)
 
-console.log("mi nombre es;", nombre, "tengo la edad de", edad, "me gusta", gusto1, "y tambien", gusto2 ${ 10 + 10 + 10 })
+console.log("mi nombre es;", nombre, "tengo la edad de", edad, "me gusta", gusto1, "y tambien", gusto2 )
 
 
 
@@ -18,4 +19,36 @@ var numero3= prompt( "ingresa el tercer dato")
 console.log(numero1+numero2+numero3);
 
 
+
+/*console.log("Hola desde JS");
+alert("Tu Pasword es Incorrecta")*/
+
+//console.log("Hola desde JS");
+
+//Metodos de salida
+console.log
+alert
+document.write
+
+//Metodos de salida
+prompt
+
+
+
+var numero1= Number (prompt("ingresa el primer dato"))
+var numero2= Number (prompt("ingresa el segundo dato"))
+var numero3= Number (prompt("ingresa el tercer dato"))
+
+console.log(numero1+numero2+numero3);
+
+
+var numero1=parseFloat {prompt( "ingresa el primer dato")}
+
+
+
+var numero1= prompt( "ingresa el primer dato")
+var numero2= prompt( "ingresa el segundo dato")
+var numero3= prompt( "ingresa el tercer dato")
+
+console.log(Number(numero1)+Number(numero2)+Number(numero3));
 
