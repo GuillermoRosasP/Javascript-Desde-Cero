2b572b8ea4a5a02cd215d29921fb0cd2dc7fec55
Guillermo Rosas <memorosaspalma@gmail.com>
2025-02-05 21:59:24 -0600

    Introduccion a javascript

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Hola desde JS");
+alert("Tu Pasword es Incorrecta")
+
+var nombre = "Guillermo"
+var edad = 27
+var gusto1= "Jugar"
+var gusto2="hacer ejercicio"
+console.log(typeof nombre)
+
+console.log("mi nombre es;", nombre, "tengo la edad de", edad, "me gusta", gusto1, "y tambien", gusto2 ${ 10 + 10 + 10 })
+
+
+
+var numero1= prompt( "ingresa el primer dato")
+var numero2= prompt( "ingresa el segundo dato")
+var numero3= prompt( "ingresa el tercer dato")
+
+console.log(numero1+numero2+numero3);
+
+
+
