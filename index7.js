44e981fd4efdaf118953256287b2a16cb460fb4e
Guillermo Rosas <memorosaspalma@gmail.com>
2025-02-27 23:50:36 -0600

    proyecto7

--- index7.js
+++ index7.js
@@ -1,5 +1,12 @@
 
 
 
-const element = document.querySelector(".container");
-  console.log(element.nodeName); // "DIV"
+function generatePassword() {
+  const length = 12;
+  const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}:";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+      password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  document.getElementById("password").value = password;
+}
